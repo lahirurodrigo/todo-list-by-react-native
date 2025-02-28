ae3d4f59f95772934b94eb3d4a650a0dd41492f2
lahirurodrigo <kmlahirurodrigo@gmail.com>
2025-02-28 18:31:13 +0530

    add login page and layout for that

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,19 +1,14 @@
-import { Stack } from 'expo-router';
+import {Stack} from "expo-router";
+import {Provider} from "react-redux";
+import {store} from "@/app/redux/store";
 
 export default function RootLayout() {
-  return (
-    <Stack
-      screenOptions={{
-        headerStyle: {
-          backgroundColor: '#f4511e',
-        },
-        headerTintColor: '#fff',
-        headerTitleStyle: {
-          fontWeight: 'bold',
-        },
-      }}>
-      <Stack.Screen name="index" />
-      <Stack.Screen name="details" />
-    </Stack>
-  );
+    return (
+        <Provider store={store}>
+            <Stack screenOptions={{ headerShown: false }} >
+                <Stack.Screen name='index' options={{headerShown: false}} />
+                <Stack.Screen name='todo' options={{headerShown: false}}/>
+            </Stack>
+        </Provider>
+    );
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,17 +1,43 @@
-import { View, Text, StyleSheet } from 'react-native';
+import {Button, StyleSheet, Text, TextInput, View} from "react-native";
+import {useState} from "react";
+import {useRouter} from "expo-router";
 
-export default function HomeScreen() {
+export default function LoginScreen() {
+
+  const router = useRouter();
+
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  function handleLogin(){
+    if(username === 'User' && password === 'user'){
+      router.replace('/screens/todo');
+    }
+  }
   return (
-    <View style={styles.container}>
-      <Text>Home</Text>
-    </View>
+      <View style={styles.container}>
+        <Text style={styles.loginText}>Login</Text>
+        <TextInput style={styles.textFields} placeholder='Username' onChangeText={setUsername}/>
+        <TextInput style={styles.textFields} placeholder='Password' secureTextEntry onChangeText={setPassword}/>
+        <Button onPress={handleLogin} title='Login'/>
+      </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+  container :{
+    flex:1,
+    justifyContent: "center",
+    padding: 20
+  },
+  loginText: {
+    fontSize: 24,
+    fontWeight: "bold",
+    marginBottom: 20
+  },
+  textFields :{
+    borderWidth: 1,
+    padding: 10,
+    marginBottom : 10
   },
 });
